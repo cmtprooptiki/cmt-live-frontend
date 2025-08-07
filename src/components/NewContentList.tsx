0bbf8067508b3c54a22db302f82afa3afc70da72
Ippokratis Geranakis <igeran@cmt.local>
2025-08-07 15:57:17 +0300

    Strapi changes

--- src/components/NewContentList.tsx
+++ src/components/NewContentList.tsx
@@ -54,6 +54,8 @@ export function NewContentList({
     right: "text-right",
   }[headlineAlignment];
 
+  console.log("Articles",articles)
+
   return (
     <section className="w-full max-w-7xl mx-auto px-4 py-10">
       <h3 className={`text-3xl font-bold mb-8 ${alignmentClass}`}>
