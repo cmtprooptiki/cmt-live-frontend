90edce7fd85092765c2fc5dbd5c7de6ce854fd57
Ippokratis Geranakis <igeran@cmt.local>
2025-08-20 13:01:50 +0300

    NewsLetter actions created

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -119,7 +119,7 @@ export function Footer({ data }: { data: FooterProps }) {
             <input
               type="email"
               name="email"
-              placeholder={newsletterInputPlaceholder}
+              placeholder={errorMessage || successMessage || newsletterInputPlaceholder}
               className="w-full border px-3 py-2 mb-2 rounded"
             />
             <button type = "submit" className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-500">
