ef107465effa4505e3af01ad9e76b99cef932a2f
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2025-08-25 11:06:35 +0300

    Monday commet card

--- src/components/blocks/TeamGrid.tsx
+++ src/components/blocks/TeamGrid.tsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import { StrapiImage } from "@/components/StrapiImage";
 import { TeamGridProps } from "@/types";
 import { SocialIcon } from "react-social-icons";
+import { CometCard } from "@/components/ui/comet-card";
+
 import Link from "next/link";
 // import { ContentList} from "@/components/ContentList"
 import { NewContentList } from "@/components/NewContentList";
@@ -59,6 +61,69 @@ export  function TeamGrid({ Title, team_members, page, query, category }: Readon
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
         {paginatedMembers.map((member) => (
           <div key={member.id} className="text-center">
+
+
+
+                      <CometCard>
+                <Link href={`/team/${member.slug}`}
+                  // type="button"
+                  className="my-10 flex w-80 cursor-pointer flex-col items-stretch rounded-[16px] border-0 bg-[#1F2121] p-2 saturate-0 md:my-20 md:p-4"
+                  aria-label="View invite F7RA"
+                  style={{
+                    transformStyle: "preserve-3d",
+                    transform: "none",
+                    opacity: 1,
+                  }}
+                >
+                  <div className="mx-2 flex-1">
+                    <div className="relative mt-2 aspect-[3/4] w-full">
+                    {member.ProfileImage?.url && (
+                        <StrapiImage
+                          className="abs olute inset-0 h-full w-full rounded-[16px] bg-[#000000] object-cover contrast-75"
+
+                              src={member.ProfileImage.url}
+                              alt={
+                                member.ProfileImage?.alternativeText ||
+                                member.FullName ||
+                                "Profile Image"
+                              }
+                              width={400}
+                              height={400}
+                              // className="object-cover w-full h-full"
+                            />
+                      // <img
+                      //   loading="lazy"
+                      //   className="absolute inset-0 h-full w-full rounded-[16px] bg-[#000000] object-cover contrast-75"
+                      //   alt="Invite background"
+                      //   src={member.ProfileImage.url}
+                      //      style={{
+                      //     boxShadow: "rgba(0, 0, 0, 0.05) 0px 5px 6px 0px",
+                      //     opacity: 1,
+                      //   }}
+                      // />
+                          )}
+                    </div>
+                  </div>
+                  <div className="mt-2 flex flex-shrink-0 items-center justify-between p-4 font-mono text-white">
+                    <div className="text-xs">{member.FullName}</div>
+                    <div className="text-xs text-gray-300 opacity-50">{member.JobTitle}</div>
+                    {member.LinkedInUrl && (
+                        <div className="">
+                          <SocialIcon
+                            network="linkedin"
+                            url={member.LinkedInUrl}
+                            style={{ height: 33, width: 33 }}
+                          />
+                        </div>
+                      )}
+                  </div>
+                </Link>
+              </CometCard>
+
+
+
+
+
             {/* Profile Image */}
             {member.ProfileImage?.url && (
               <Link href={`/team/${member.slug}`}>
