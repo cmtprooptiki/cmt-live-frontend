bba51277f2e391b7a5cf19b26928d813f500fe03
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2025-09-04 18:08:45 +0300

    learder and testimonial update

--- src/components/BlockRenderer.tsx
+++ src/components/BlockRenderer.tsx
@@ -88,6 +88,7 @@ import { InfoBlock } from "@/components/blocks/InfoBlock";
 import {MilestoneBlock} from "@/components/blocks/MilestoneBlock";
 import {VerticalAccordionBlock} from "@/components/blocks/VerticalAccordionBlock";
 import {ServicesAccordionBlock} from "@/components/blocks/ServicesAccordionBlock";
+import { LeadingInstitutionBlock } from "@/components/blocks/LeadingInstitutionBlock";
 import {LogoCarouselBlock} from "@/components/blocks/LogoCarouselBlock";
 import { TestimonialsBlock } from "@/components/blocks/TestimonialsBlock";
 import { FeaturesBlock } from "@/components/blocks/FeatureBlocks";
@@ -122,6 +123,8 @@ function blockRenderer(block: Block, index: number,allBlocks: Block[],secondaryM
        return <ServicesAccordionBlock {...block} key={index}  />;
     case "blocks.logo-carousel-block":
        return <LogoCarouselBlock  {...block} key={index}  />;
+    case "blocks.leading-institution-block":
+      return <LeadingInstitutionBlock {...block} key={index} />;
     case "blocks.testimonials-block":
         return <TestimonialsBlock {...block} key={index} />;
     case "blocks.features-block" :
